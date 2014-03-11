9cb1a134095ba141a8417337792a7b64f51752e1
Prescott Murphy <prurph@gmail.com>
2014-03-11 14:13:09 -0400

    Set up new Rails app, adjust gemfile, setup database and turn off generation of helpers, assets and controller and view specs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wdi10RailsDemoAngularMine::Application.config.secret_key_base = 'a5b7080e7c6dfa7a5aeb8c1632b070969fb511fcc7b73055310e22eff57ee1adb390b4c0af77088506dc1e5bdce0f8e1f47d98b90779a44eb9df9e4bf55ce514'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wdi10RailsDemoAngularMine::Application.config.session_store :cookie_store, key: '_wdi_10_rails_demo_angular_mine_session'
